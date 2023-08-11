e796d7724235f90a440b079f1bd27f9eb59bfc23
Tyler Santos <45154554+santost12@users.noreply.github.com>
2023-08-11 13:10:44 -0400

    check network address and subnet mask

--- ipcalc.c
+++ ipcalc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 struct ipv4 {
     uint8_t octet_1;
@@ -52,20 +53,79 @@ void calculate_usable_ip(struct ipv4 *network_addr, struct ipv4 *broadcast_addr,
     }
 }
 
-int main(void)
+/*
+Valid subnet masks should have consecutive '1' bits
+followed by consecutive '0' bits.
+*/
+bool valid_subnet_mask(struct ipv4 *subnet_mask)
 {
-    struct ipv4 ip_addr;
+    /* combine the octets to 32 bit unsigned int */
+    uint32_t mask = ((uint32_t)subnet_mask->octet_1 << 24) |
+                    ((uint32_t)subnet_mask->octet_2 << 16) |
+                    ((uint32_t)subnet_mask->octet_3 << 8) |
+                    (uint32_t)subnet_mask->octet_4;
+
+    bool consecutive_zeros = false;
+
+    for (int i = 31; i >= 0; i--) {
+        if (mask & (1 << i)) {
+            if (consecutive_zeros) {
+                return false;
+            }
+        } else {
+            consecutive_zeros = true;
+        }
+    }
+
+    return true;
+}
+
+/*
+Check if valid network address by using the bitwise AND operation between
+the network address and the inverted subnet mask.
+*/
+bool valid_network_addr(struct ipv4 *network_addr, struct ipv4 *subnet_mask)
+{
+    struct ipv4 calculated_network_addr;
+
+    calculated_network_addr.octet_1 = network_addr->octet_1 & subnet_mask->octet_1;
+    calculated_network_addr.octet_2 = network_addr->octet_2 & subnet_mask->octet_2;
+    calculated_network_addr.octet_3 = network_addr->octet_3 & subnet_mask->octet_3;
+    calculated_network_addr.octet_4 = network_addr->octet_4 & subnet_mask->octet_4;
+
+    bool is_valid = 
+        calculated_network_addr.octet_1 == network_addr->octet_1 &&
+        calculated_network_addr.octet_2 == network_addr->octet_2 &&
+        calculated_network_addr.octet_3 == network_addr->octet_3 &&
+        calculated_network_addr.octet_4 == network_addr->octet_4;
+
+    return is_valid;
+}
+
+
+int main(int argc, char *argv[])
+{
+/*
+    if (argc < 3) {
+        printf("Error: Too few arguments.\n");
+        printf("Example: ipcalc 192.168.0.0 255.255.255.0\n");
+        return -1;
+    } else if (argc > 3) {
+        printf("Error: Too many arguments.\n");
+        printf("Example: ipcalc 192.168.0.0 255.255.255.0\n");
+        return -1;
+    }
+
+    printf("[debug] First argument is: %s\n", argv[1]);
+    printf("[debug] Second argument is: %s\n\n", argv[2]);
+*/
+
     struct ipv4 network_addr;
     struct ipv4 subnet_mask;
     struct ipv4 broadcast_addr;
     struct ipv4 first_usable_addr;
     struct ipv4 last_usable_addr;
 
-    /* 192 168 0 1 */
-    ip_addr.octet_1 = 0b11000000;
-    ip_addr.octet_2 = 0b10101000;
-    ip_addr.octet_3 = 0b00000000;
-    ip_addr.octet_4 = 0b00000001;
 
     /* 192 168 0 0 */
     network_addr.octet_1 = 0b11000000;
@@ -79,11 +139,25 @@ int main(void)
     subnet_mask.octet_3 = 0b11111111;
     subnet_mask.octet_4 = 0b00000000;
 
+/*
+    We need to check if the subnet mask is valid before
+    we can check if the network address is valid.    
+*/
+    if (valid_subnet_mask(&subnet_mask) == true) {
+        calculate_broadcast(&network_addr, &subnet_mask, &broadcast_addr);
+    } else {
+        printf("Error: Invalid subnet mask\n");
+        return -2;
+    }
 
-    calculate_broadcast(&network_addr, &subnet_mask, &broadcast_addr);
-    calculate_usable_ip(&network_addr, &broadcast_addr, &first_usable_addr, &last_usable_addr);
 
-    printf("IP address: %u.%u.%u.%u\n", ip_addr.octet_1, ip_addr.octet_2, ip_addr.octet_3, ip_addr.octet_4);
+    if (valid_network_addr(&network_addr, &subnet_mask) == true) {
+        calculate_usable_ip(&network_addr, &broadcast_addr, &first_usable_addr, &last_usable_addr);
+    } else {
+        printf("Error: Invalid network address for the subnet mask you provided\n");
+        return -3;
+    }
+
     printf("Network address: %u.%u.%u.%u\n", network_addr.octet_1, network_addr.octet_2, network_addr.octet_3, network_addr.octet_4);
     printf("Subnet mask: %u.%u.%u.%u\n", subnet_mask.octet_1, subnet_mask.octet_2, subnet_mask.octet_3, subnet_mask.octet_4);
     printf("Broadcast address: %u.%u.%u.%u\n", broadcast_addr.octet_1, broadcast_addr.octet_2, broadcast_addr.octet_3, broadcast_addr.octet_4);
