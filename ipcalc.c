514720c0322dbef9fddf7f7a545c4ffbf3927142
santost12 <45154554+santost12@users.noreply.github.com>
2023-11-12 17:25:12 -0500

    spacing

--- ipcalc.c
+++ ipcalc.c
@@ -4,12 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 struct ipv4 {
     uint8_t octet_1;
     uint8_t octet_2;
     uint8_t octet_3;
     uint8_t octet_4;
 };
+
 
 /*
     1) perform bitwise NOT operation on subnet mask
@@ -23,9 +25,10 @@ void calculate_broadcast(struct ipv4 *network_addr, struct ipv4 *subnet_mask, st
     broadcast_addr -> octet_4 = ~subnet_mask -> octet_4 | network_addr -> octet_4;
 }
 
+
 /*
     The first usable IP address is the network address + 1,
-    and the last usable IP address is the broadcast address - 1. 
+    and the last usable IP address is the broadcast address - 1.
 */
 void calculate_usable_ip(struct ipv4 *network_addr, struct ipv4 *broadcast_addr, struct ipv4 *first_usable_addr, struct ipv4 *last_usable_addr)
 {
@@ -43,17 +46,18 @@ void calculate_usable_ip(struct ipv4 *network_addr, struct ipv4 *broadcast_addr,
         last_usable_addr->octet_3--;
         last_usable_addr->octet_4 = 0b11111111 + last_usable_addr->octet_4;
     }
-    
+
     if (last_usable_addr->octet_3 < 0b00000000) {
         last_usable_addr->octet_2--;
         last_usable_addr->octet_3 = 0b11111111 + last_usable_addr->octet_3;
     }
-    
+
     if (last_usable_addr->octet_2 < 0b00000000) {
         last_usable_addr->octet_1--;
         last_usable_addr->octet_2 = 0b11111111 + last_usable_addr->octet_2;
     }
 }
+
 
 /*
     Valid subnet masks should have consecutive '1' bits
@@ -66,7 +70,7 @@ bool valid_subnet_mask(struct ipv4 *subnet_mask)
                     ((uint32_t)subnet_mask->octet_2 << 16) |
                     ((uint32_t)subnet_mask->octet_3 << 8) |
                     (uint32_t)subnet_mask->octet_4;
-    
+
     bool consecutive_zeros = false;
 
     for (int i = 31; i >= 0; i--) {
@@ -79,6 +83,7 @@ bool valid_subnet_mask(struct ipv4 *subnet_mask)
 
     return consecutive_zeros;
 }
+
 
 /*
     Check if valid network address by using the bitwise AND operation between
@@ -93,7 +98,7 @@ bool valid_network_addr(struct ipv4 *network_addr, struct ipv4 *subnet_mask)
     calculated_network_addr.octet_3 = network_addr->octet_3 & subnet_mask->octet_3;
     calculated_network_addr.octet_4 = network_addr->octet_4 & subnet_mask->octet_4;
 
-    bool is_valid = 
+    bool is_valid =
         calculated_network_addr.octet_1 == network_addr->octet_1 &&
         calculated_network_addr.octet_2 == network_addr->octet_2 &&
         calculated_network_addr.octet_3 == network_addr->octet_3 &&
@@ -102,6 +107,7 @@ bool valid_network_addr(struct ipv4 *network_addr, struct ipv4 *subnet_mask)
     return is_valid;
 }
 
+
 void ipv4_to_binary(const char *ipv4_str, struct ipv4 *ipv4_addr) {
     char temp_str[16];
     strncpy(temp_str, ipv4_str, sizeof(temp_str));
@@ -109,7 +115,7 @@ void ipv4_to_binary(const char *ipv4_str, struct ipv4 *ipv4_addr) {
 
     char *token = strtok(temp_str, ".");
     ipv4_addr->octet_1 = atoi(token);
-    
+
     for (int i = 2; i <= 4; i++) {
         token = strtok(NULL, ".");
         if (token == NULL) {
@@ -133,7 +139,7 @@ int main(int argc, char *argv[])
         printf("Usage: %s network_id subnet_mask\n", argv[0]);
         return -1;
     }
-    
+
     struct ipv4 network_addr;
     struct ipv4 subnet_mask;
     struct ipv4 broadcast_addr;
